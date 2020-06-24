0a220056960de4a5752af09b0f66d7d00230f643
Daniel Slobodscoy <danielslobodscoy@gmail.com>
2020-06-24 15:09:14 -0400

    questions done

---
+++ index.js
@@ -0,0 +1,71 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const api = require("./utils/api.js");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const axios = require("axios");
+
+//Questions
+function questions(){
+ return inquirer.prompt([
+    {
+        message: "Enter your GitHub username:",
+        name: "Username"
+    },
+    {
+        message: "What is the title of your project ",
+        name: "Title"
+    },
+    {
+        message: "Description of your project in a few words",
+        name: "Description"
+    },
+    {
+        type: "input",
+        name: "contents",
+        message: "What are the contents of the project?"
+    },
+    {
+        message: 'What are the necessary packages required to be installed to run the application? ',
+        name: 'Installation'
+    },
+    {
+        message: "How to use the application ",
+        name: "Usage"
+    },
+    {
+        message: "Provide the license of the application",
+        name: "License"
+    },
+    {
+        messages: "list all the contributors for the project",
+        name: "Contributors"
+    },
+    {
+        type: "input",
+        name: "Test",
+        message: "How to test the application to make sure it is running?"
+    }
+])}
+
+function generate(data,me) {
+    fs.writeFile('README.md', generateMarkdown(data, me), (err) => {
+        if (err) {
+            throw err;
+        }
+    })
+}
+
+async function init() {
+    console.log("hi")
+    try {
+        const answers = await questions();
+        const README = await api(answers.username);
+        await generate(answers, README);
+
+        console.log("Successfully generated README.md");
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,38 @@
+function generateMarkdown(data, response) {
+  return `
+# ${data.title}
+# Table of Contents:
+1. [Description](#description)
+2. [Contents](#contents)
+3. [Installation](#installation)
+4. [Usage](#usage)
+5. [Licensing](#license)nod
+6. [Contributions](#contributions)
+7. [Test](#test)
+8. [Questions](#questions)
+## Description
+${data.Description}
+## Contents
+${data.contents}
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+${data.license}
+## Contributions
+${data.contributions}
+## Test
+${data.test}
+## Questions
+${data.questions}
+## Badge(s)
+![Badge](${data.badge})
+## Questions
+<img src="${response.data.avatar_url}" height="100" width="75">
+
+${response.data.email}
+`;
+}
+
+module.exports = generateMarkdown;
